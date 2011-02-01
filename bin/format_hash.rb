ac84b6b0198a0c85c2ca27bb973f36edc450cc28
Xavier Shay <xavier@rhnh.net>
2011-02-01 19:14:25 +1100

    Format ruby 1.9 style hashes

--- bin/format_hash.rb
+++ bin/format_hash.rb
@@ -19,9 +19,18 @@
 # ab  = 2
 # abc = 3
 #
+# a: 1
+# ab: 2
+# abc: 3
+#
+# becomes
+# a:   1
+# ab:  2
+# abc: 3
+#
 # From github.com/xaviershay/dotfiles
 
-OPERATOR_RE = /=>|=/
+OPERATOR_RE = /\:|=>|=/
 
 lines    = readlines
 operator = nil
@@ -39,8 +48,13 @@ max_key_length = lines.map {|line| line[0].length}.max
 
 # Pad each key with whitespace to match length of longest key
 lines.map! do |line|
-  line[0] = "%#{indent}s%-#{max_key_length}s" % ["", line[0]]
-  line.join(" #{operator} ")
+  if operator == ':'
+    line[0] = "%#{indent}s%-#{max_key_length + 1}s" % ["", line[0] + operator]
+    line.join(" ")
+  else
+    line[0] = "%#{indent}s%-#{max_key_length}s" % ["", line[0]]
+    line.join(" #{operator} ")
+  end
 end
 
 print lines.join("\n")
